9dbe35509e373e03a7921e3c2b0b4116b444d660
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2014-10-10 15:38:36 -0500

    fixes

--- site-02/js/main.js
+++ site-02/js/main.js
@@ -16,7 +16,7 @@ $(function() {
 
 //
 
-var a_phone_number = '+1 (224) 678-1608';
+var a_phone_number = '+1-224-678-1608';
 var a_phone_url = 'tel:' + a_phone_number;
 
 var a_skype_address = 'Marina.Narubin';
@@ -26,7 +26,7 @@ var a_email_address = 'Marina.Narubin@gmail.com';
 var a_email_subject = 'MarinaSpeech Consultation';
 var a_email_message = 'Please call us back at ...';
 var a_email_url = 'mailto:' + a_email_address + '?' + "subject="
-		+ encodeURIComponent(a_email_subject) + '&' + 'body='
-		+ encodeURIComponent(a_email_message);
+		+ encodeURI(a_email_subject) + '&' + 'body='
+		+ encodeURI(a_email_message);
 
 var a_online_url = 'https://docs.google.com/forms/d/1MLcNg3Z7TQT8i-xvqYq-jYmCljkxvLSgIxPnR6uanaM/viewform';
