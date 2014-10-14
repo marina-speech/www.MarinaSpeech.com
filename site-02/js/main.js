4ec63c50440c64bcb7bfdbf03e3dcdf5f0a7b053
Marina Narubin <Marina.Narubin@gmail.com>
2014-10-14 10:47:15 -0500

    change skype name

--- site-02/js/main.js
+++ site-02/js/main.js
@@ -19,7 +19,7 @@ $(function() {
 var a_phone_number = '+1-224-678-1608';
 var a_phone_url = 'tel:' + a_phone_number;
 
-var a_skype_address = 'Marina.Narubin';
+var a_skype_address = 'Marina.Speech';
 var a_skype_url = 'skype:' + a_skype_address;
 
 var a_email_address = 'Marina.Narubin@gmail.com';
